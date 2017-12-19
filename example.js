b114dd5640be55c1b4b6889844278051dbc0a70d
samelie <sam@stinkdigital.com>
2017-12-19 13:32:39 -0800

    working fb

--- example.js
+++ example.js
@@ -1,13 +1,18 @@
 const raf = require("raf")
 var toBuffer = require("typedarray-to-buffer")
 const ffmpeg = require("./lib/ffmpeg")
+const FB = require("./lib/fb")
 const GL = require("./lib/gl")
-/*const WIDTH = 640
-const HEIGHT = 480*/
-const WIDTH = 352
-const HEIGHT = 288
+const BITRATE_A = 128
+const BITRATE_V = 600
+const FPS = 30
+const WIDTH = 640
+const HEIGHT = 480
+/*const WIDTH = 352
+const HEIGHT = 288*/
 var now = require("performance-now")
 const WebcamWebsocket = require("./index")
+const WEBCAM_IPS = ["10.0.1.9"]
 const WEBCAM_IP = "10.0.1.7"
 const WEBCAM_IP_2 = "10.0.1.9"
 const STREAM_IP = "10.0.1.8"
@@ -15,16 +20,17 @@ const web = WebcamWebsocket()
 
 let FFMPEG
 function startStream(options) {
-  let maxrate = options.maxrate || 400
+  let maxrate = options.maxrate || 600
   FFMPEG = ffmpeg({
-    format: "mpegts",
+    ...options,
     w: options.w,
     h: options.h,
-    output: `udp://${options.ip}:${options.port}`,
-    options: [
-      ` -preset ultrafast -tune zerolatency  -b:v ${maxrate /
-        2}k -maxrate ${maxrate}k -bufsize ${maxrate}k  `,
-    ],
+    output: options.output || `udp://${options.ip}:${options.port}`,
+    options: [`${options.options || ""} `],
+
+    /*
+
+    */
   })
 }
 
@@ -32,177 +38,112 @@ function startStream(options) {
   ip: STREAM_IP,
   port: "1337",
 })*/
-/*
-const SHADER_FRAGMENT_YCBCRTORGBA = [
-    "precision lowp float;",
-    "uniform sampler2D tex0;",
-    "uniform sampler2D tex1;",
-    "varying vec2 texCoord;",
-
-    "void main() {",
-    "vec3 cr0 = texture2D(tex0, texCoord).rgb;",
-    "vec3 cr1 = texture2D(tex1, texCoord).rgb;",
-    "vec3 c = mix(cr0,cr1,0.5);",
-
-    "gl_FragColor = vec4(cr1,1);",
-
-    "}",
-  ].join("\n"),
-  SHADER_VERTEX_IDENTITY = [
-    "attribute vec2 vertex;",
-    "varying vec2 texCoord;",
-
-    "void main() {",
-    "texCoord = vertex;",
-    "gl_Position = vec4((vertex * 2.0 - 1.0) * vec2(1, 1), 0.0, 1.0);",
-    "}",
-  ].join("\n")
-let _texture1, _texture2, program
-
-const createTexture = (index, name) => {
-  var gl = GL
-  var texture = gl.createTexture()
-
-  gl.bindTexture(gl.TEXTURE_2D, texture)
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-  gl.uniform1i(gl.getUniformLocation(program, name), index)
-
-  return texture
-}*/
-
-/*
-const renderWebgl = (tex0B, tex1B) => {
-  var gl = GL
-
-  gl.activeTexture(gl.TEXTURE0)
-  gl.bindTexture(gl.TEXTURE_2D, _texture1)
-
-  gl.texImage2D(
-    gl.TEXTURE_2D,
-    0,
-    gl.RGBA,
-    W,
-    H,
-    0,
-    gl.RGBA,
-    gl.UNSIGNED_BYTE,
-    tex0B
-  )
-
-  gl.activeTexture(gl.TEXTURE1)
-  gl.bindTexture(gl.TEXTURE_2D, _texture2)
-  gl.texImage2D(
-    gl.TEXTURE_2D,
-    1,
-    gl.RGBA,
-    W,
-    H,
-    0,
-    gl.RGBA,
-    gl.UNSIGNED_BYTE,
-    tex1B
-  )
-
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
-}
-
-const compileShader = (type, source) => {
-  var gl = GL
-  var shader = gl.createShader(type)
-  gl.shaderSource(shader, source)
-  gl.compileShader(shader)
-
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    throw new Error(gl.getShaderInfoLog(shader))
-  }
-
-  return shader
-}*/
-/*
-const initWebgl = () => {
-  // attempt to get a webgl context
-  var gl = GL
-  // init buffers
-  var buffer = gl.createBuffer()
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
-  gl.bufferData(
-    gl.ARRAY_BUFFER,
-    new Float32Array([0, 0, 0, 1, 1, 0, 1, 1]),
-    gl.STATIC_DRAW
-  )
-
-  // The main YCbCrToRGBA Shader
-  program = gl.createProgram()
-  gl.attachShader(
-    program,
-    compileShader(gl.VERTEX_SHADER, SHADER_VERTEX_IDENTITY)
-  )
-  gl.attachShader(
-    program,
-    compileShader(gl.FRAGMENT_SHADER, SHADER_FRAGMENT_YCBCRTORGBA)
-  )
-  gl.linkProgram(program)
-
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    throw new Error(gl.getProgramInfoLog(program))
-  }
-
-  gl.useProgram(program)
-
-  // setup textures
-  _texture1 = createTexture(0, "tex0")
-  _texture2 = createTexture(1, "tex1")
-
-  var vertexAttr = gl.getAttribLocation(program, "vertex")
-  gl.enableVertexAttribArray(vertexAttr)
-  gl.vertexAttribPointer(vertexAttr, 2, gl.FLOAT, false, 0, 0)
-}*/
 
 const gl = GL({
   width: WIDTH,
   height: HEIGHT,
 })
 
+const connections = WEBCAM_IPS.map(ip =>
+  web.connect(gl, ip, {
+    ip: STREAM_IP,
+    port: "1337",
+  })
+)
+
 var _t = now().toFixed(3)
-const fps = 2000
 var handle = raf(function tick() {
   var start = now().toFixed(3)
 
-  if (start - _t >= 80) {
+  if (start - _t >= 25 && FFMPEG) {
     //&& ff2.player.outBuffer
-    if (ff.player.outBuffer) {
-      //console.log(ff2.player.outBuffer.buffer);
-      /*return gl.read(new Uint8Array(WIDTH * HEIGHT * 4))
+
+    if (WEBCAM_IPS.length == 1) {
+      if (connections[0].player.outBuffer) {
+        gl.drawSingle({
+          tex0: connections[0].player.pixels,
+        })
+        FFMPEG.frame(toBuffer(gl.read(WIDTH, HEIGHT)))
+      }
+    } else if (WEBCAM_IPS.length == 2) {
+      if (
+        connections[0].player.outBuffer &&
+        connections[1].player.outBuffer
+      ) {
+        //console.log(ff2.player.outBuffer.buffer);
+        /*return gl.read(new Uint8Array(WIDTH * HEIGHT * 4))
 
       renderWebgl(ff.player.outBuffer, ff2.player.outBuffer)
       var pixels = new Uint8Array(W * H * 4)
       GL.readPixels(0, 0, W, H, GL.RGBA, GL.UNSIGNED_BYTE, pixels)*/
-      //FFMPEG.frame(toBuffer(ff2.player.outBuffer))
+        //FFMPEG.frame(toBuffer(ff2.player.outBuffer))
+        gl.mergeStreams({
+          tex0: connections[0].player.pixels,
+          tex1: connections[1].player.pixels,
+        })
+        FFMPEG.frame(toBuffer(gl.read(WIDTH, HEIGHT)))
+      }
     }
-    FFMPEG.frame(toBuffer(gl.read(WIDTH, HEIGHT)))
     _t = start
   }
   raf(tick)
 })
 
-const ff = web.connect(gl, WEBCAM_IP_2, {
+/*const ff = web.connect(gl, WEBCAM_IP_2, {
   ip: STREAM_IP,
   port: "1337",
 })
+*/
 
-startStream(
-  Object.assign(
-    {},
-    {
-      ip: STREAM_IP,
-      port: "1337",
-    },
-    { w: WIDTH, h: HEIGHT }
-  )
-)
+function start() {
+  var privacys = {
+    public: "{'value':'EVERYONE'}",
+    friends: "{'value':'ALL_FRIENDS'}",
+    friends_of_friends: "{'value':'FRIENDS_OF_FRIENDS'}",
+  }
+  FB.startLiveVideo({
+    accessToken:
+      "EAAXiyxq1MwkBAOg8Qi5h17SIPtgkG0dfiFxn0u5kjTqC2YVNtZC9Cnd8lVWsmIZB3P4xmccCPpZB9XhKe1MC7yOTXmhL96xkgkB4GAZBiwiQkR4FNbVkKbZBydJ2zdSYVUFOQhsy6FtNh66HYTKz1ys74Oa4vPSEZD",
+    title: "Live Video",
+    /*privacy:
+      privacys["public"] ||
+      "{'value':'CUSTOM',allow:'100009508046151,1751806573'}",*/
+  })
+    .then(liveVideo => {
+      console.log(liveVideo)
+
+      var rtmpUrl = liveVideo.stream_url
+      postId = liveVideo.id
+
+      console.log("postId", postId)
+
+      startStream(
+        Object.assign(
+          {},
+          {
+            ip: STREAM_IP,
+            port: "1337",
+            format: "flv",
+            //input: [],
+            //input: ["-f", "lavfi", "-i", "anullsrc"],
+            input: ["-y", "-f", "avfoundation", "-i", ":2"],
+            //input: ["-f", "alsa", "-ac", "1", "-ar", "44100" ,"-i", "hw:3"],
+            //input: ["-i", "hw:1,0","-f alsa", "-ac", "2",],
+            options:
+              `-b:a ${BITRATE_A}k -c:v libx264 -profile:v baseline -pix_fmt yuv420p -g ${FPS * 2} -r ${FPS} -f flv -preset ultrafast -tune zerolatency  -b:v ${BITRATE_V}k -minrate ${BITRATE_V / 2}k  -maxrate ${BITRATE_V}k  -bufsize ${BITRATE_V}k`,
+            //output: `"rtmp://a.rtmp.youtube.com/live2/f5v7-kfmq-27ce-9dft"`, //`"${rtmpUrl}"`,
+            output:`"${rtmpUrl}"`
+          },
+          { w: WIDTH, h: HEIGHT }
+        )
+      )
+    })
+    .catch(error => {
+      console.error(error.message, error.options)
+    })
+}
+
+start()
 
 //initWebgl()
 
