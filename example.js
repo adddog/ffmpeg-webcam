80fc9f87e197896d9643ff9f63c4f788a80ee722
samelie <sam@stinkdigital.com>
2017-12-20 14:05:49 -0800

    progress with max, but basic still working

--- example.js
+++ example.js
@@ -16,22 +16,30 @@ const HEIGHT = 288
 
 const OFFLINE = false
 
+const TCP = false
+const TCP_STREAM_NAME = "/webcam"
+
 var now = require("performance-now")
 const WebcamWebsocket = require("./index")
-const WEBCAM_IPS = ["10.0.1.9", "10.0.1.3"]
+const WEBCAM_IPS = ["10.0.1.9"] //, "10.0.1.3"
 const WEBCAM_IP = "10.0.1.7"
 const WEBCAM_IP_2 = "10.0.1.9"
 const STREAM_IP = "10.0.1.8"
+const STREAM_PORT = "1337"
 const web = WebcamWebsocket()
 
 let FFMPEG
 function startStream(options) {
   let maxrate = options.maxrate || 600
+  console.log(
+    options.output || `"udp://${options.ip}:${options.port}"`
+  )
+
   FFMPEG = ffmpeg({
     ...options,
     w: options.w,
     h: options.h,
-    output: options.output || `udp://${options.ip}:${options.port}`,
+    output: options.output,
     options: [`${options.options || ""} `],
 
     /*
@@ -50,7 +58,7 @@ const GL_UNIFORMS = {
   tolerance: 0.5,
   keyIndex: 1,
   keyColor: [0, 0, 0],
-  uSaturations: [1,1,1,1],
+  uSaturations: [1, 1, 1, 1],
 
   selectionIndex: 0,
 }
@@ -59,6 +67,8 @@ const gl = GL({
   width: WIDTH,
   height: HEIGHT,
 })
+
+const feedback = gl.regl.texture()
 
 const connections = WEBCAM_IPS.map(ip =>
   web.connect(gl, ip, {
@@ -96,12 +106,15 @@ var handle = raf(function tick() {
         gl.mergeStreams({
           tex0: connections[0].player.pixels,
           tex1: connections[1].player.pixels,
+          feedback: feedback,
           slope: GL_UNIFORMS.slope,
           tolerance: GL_UNIFORMS.tolerance,
           keyIndex: GL_UNIFORMS.keyIndex,
           keyColor: GL_UNIFORMS.keyColor,
           uSaturations: GL_UNIFORMS.uSaturations,
-
+        })
+        feedback({
+          copy: true,
         })
         FFMPEG.frame(toBuffer(gl.read(WIDTH, HEIGHT)))
       }
@@ -117,55 +130,77 @@ var handle = raf(function tick() {
 })
 */
 
-function start() {
-  var privacys = {
-    public: "{'value':'EVERYONE'}",
-    friends: "{'value':'ALL_FRIENDS'}",
-    friends_of_friends: "{'value':'FRIENDS_OF_FRIENDS'}",
-  }
-  FB.startLiveVideo({
-    accessToken:
-      "EAAXiyxq1MwkBAOg8Qi5h17SIPtgkG0dfiFxn0u5kjTqC2YVNtZC9Cnd8lVWsmIZB3P4xmccCPpZB9XhKe1MC7yOTXmhL96xkgkB4GAZBiwiQkR4FNbVkKbZBydJ2zdSYVUFOQhsy6FtNh66HYTKz1ys74Oa4vPSEZD",
-    title: "Live Video",
-    /*privacy:
-      privacys["public"] ||
-      "{'value':'CUSTOM',allow:'100009508046151,1751806573'}",*/
-  })
-    .then(liveVideo => {
-      console.log(liveVideo)
+const startFFMPEG = rtmpUrl => {
+  const _videoBitrate = ` -preset ultrafast -tune zerolatency  -b:v ${BITRATE_V}k -minrate ${BITRATE_V /
+    2}k  -maxrate ${BITRATE_V}k  -bufsize ${BITRATE_V}k`
 
-      var rtmpUrl = liveVideo.stream_url
-      postId = liveVideo.id
-      const _videoBitrate = ` -preset ultrafast -tune zerolatency  -b:v ${BITRATE_V}k -minrate ${BITRATE_V /
-        2}k  -maxrate ${BITRATE_V}k  -bufsize ${BITRATE_V}k`
-      const _options = OFFLINE
-        ? ` -f mpegts `
-        : ` -b:a ${BITRATE_A}k -c:v libx264 -profile:v baseline -pix_fmt yuv420p -g ${FPS *
-            2} -r ${FPS} -f flv `
-      startStream(
-        Object.assign(
-          {},
-          {
-            ip: STREAM_IP,
-            port: "1337",
-            //input: [],
-            //input: ["-f", "lavfi", "-i", "anullsrc"],
-            input: OFFLINE
-              ? null
-              : ["-y", "-f", "avfoundation", "-i", ":2"],
-            //input: ["-f", "alsa", "-ac", "1", "-ar", "44100" ,"-i", "hw:3"],
-            //input: ["-i", "hw:1,0","-f alsa", "-ac", "2",],
-            options: `${_options} ${_videoBitrate}`,
-            //output: `"rtmp://a.rtmp.youtube.com/live2/f5v7-kfmq-27ce-9dft"`, //`"${rtmpUrl}"`,
-            output: OFFLINE ? null : `"${rtmpUrl}"`,
-          },
-          { w: WIDTH, h: HEIGHT }
-        )
-      )
+  //-fflags nobuffer
+
+  const _options = OFFLINE
+    ? `${TCP
+        ? " -acodec aac -strict -2 -ar 48000 -ab 96k "
+        : ""} -g ${Math.round(FPS / 4)} -r ${FPS} ${TCP ? "" : " "} `
+    : ` -b:a ${BITRATE_A}k -c:v libx264 -profile:v baseline -pix_fmt yuv420p -g ${FPS /
+        4} -r ${FPS} `
+
+  const _format = OFFLINE ? `${TCP ? "" : " -f mpegts"} ` : ` -f flv `
+
+  const _audioInput = OFFLINE
+    ? ["-f", "lavfi", "-i", "anullsrc"]
+    : ["-y", "-f", "avfoundation", "-i", ":3"]
+  startStream(
+    Object.assign(
+      {},
+      {
+        //input: [],
+        input: _audioInput,
+        // input: OFFLINE
+        //   ? null
+        //   : ["-y", "-f", "avfoundation", "-i", ":3"],
+        //input: ["-f", "alsa", "-ac", "1", "-ar", "44100" ,"-i", "hw:3"],
+        //input: ["-i", "hw:1,0","-f alsa", "-ac", "2",],
+        options: `${_options} ${_videoBitrate} ${_format}`,
+        //output: `"rtmp://a.rtmp.youtube.com/live2/f5v7-kfmq-27ce-9dft"`, //`"${rtmpUrl}"`,
+        output: OFFLINE
+          ? `${TCP
+              ? `"http://127.0.0.1:8080${TCP_STREAM_NAME}.ffm"`
+              : `"udp://${STREAM_IP}:${STREAM_PORT}"`}`
+          : `"${rtmpUrl}"`,
+      },
+      { w: WIDTH, h: HEIGHT }
+    )
+  )
+}
+
+function start() {
+  if (OFFLINE) {
+    startFFMPEG()
+  } else {
+    var privacys = {
+      public: "{'value':'EVERYONE'}",
+      friends: "{'value':'ALL_FRIENDS'}",
+      friends_of_friends: "{'value':'FRIENDS_OF_FRIENDS'}",
+    }
+    FB.startLiveVideo({
+      accessToken:
+        "EAAXiyxq1MwkBAOg8Qi5h17SIPtgkG0dfiFxn0u5kjTqC2YVNtZC9Cnd8lVWsmIZB3P4xmccCPpZB9XhKe1MC7yOTXmhL96xkgkB4GAZBiwiQkR4FNbVkKbZBydJ2zdSYVUFOQhsy6FtNh66HYTKz1ys74Oa4vPSEZD",
+      title: "Live Video",
+      /*privacy:
+        privacys["public"] ||
+        "{'value':'CUSTOM',allow:'100009508046151,1751806573'}",*/
     })
-    .catch(error => {
-      console.error(error.message, error.options)
-    })
+      .then(liveVideo => {
+        console.log(liveVideo)
+
+        var rtmpUrl = liveVideo.stream_url
+        postId = liveVideo.id
+
+        startFFMPEG(rtmpUrl)
+      })
+      .catch(error => {
+        console.error(error.message, error.options)
+      })
+  }
 }
 
 start()
@@ -204,20 +239,14 @@ process.stdin.on("keypress", (str, key) => {
       break
 
     case "y":
-      GL_UNIFORMS.uSaturations[
-        GL_UNIFORMS.selectionIndex
-      ] = Math.min(
-        GL_UNIFORMS.uSaturations[GL_UNIFORMS.selectionIndex] +
-          0.05,
+      GL_UNIFORMS.uSaturations[GL_UNIFORMS.selectionIndex] = Math.min(
+        GL_UNIFORMS.uSaturations[GL_UNIFORMS.selectionIndex] + 0.05,
         4
       )
       break
     case "h":
-      GL_UNIFORMS.uSaturations[
-        GL_UNIFORMS.selectionIndex
-      ] = Math.max(
-        GL_UNIFORMS.uSaturations[GL_UNIFORMS.selectionIndex] -
-          0.05,
+      GL_UNIFORMS.uSaturations[GL_UNIFORMS.selectionIndex] = Math.max(
+        GL_UNIFORMS.uSaturations[GL_UNIFORMS.selectionIndex] - 0.05,
         0
       )
       break
--- readme.md
+++ readme.md
@@ -1,2 +1,21 @@
 Requiremnts: 
 
+From Max
+
+Working -STREAM RTP OUT
+You must write the sdp to a file and read that with ffplay
+
+https://cycling74.com/forums/broken-tutorial-53-jitter-networking-part-2-how-do-i-get-the-rtsp-address/
+
+https://lists.ffmpeg.org/pipermail/ffmpeg-user/2016-February/030853.html
+ffplay  -protocol_whitelist file,udp,rtp -i spd_file.spd
+
+
+MAX will play ideas.
+
+
+ffserver -f ffserver.conf
+
+stream TCP has been seen to work. rtsp seems fastest.
+
+http://peterelsea.com/Maxtuts_jitter/Jitter_with_net_cameras.pdf
